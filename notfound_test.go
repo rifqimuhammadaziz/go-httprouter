84726f4e3391a95fa8bf7064bee453d23881906e
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-18 09:54:12 +0700

    add TestNotFound for customize output notfound url

---
+++ notfound_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotFound(t *testing.T) {
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "Website not found.")
+	})
+
+	// create testing
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	// assert.Equal(t, ExpectedResult, Result)
+	assert.Equal(t, "Website not found.", string(body))
+}
