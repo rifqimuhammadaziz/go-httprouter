61056b8f62343f90e9ebdb97cc41ce192b17a2a9
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-18 09:34:36 +0700

    add TestServeFile and add resources directory

---
+++ serve_file_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	// create testing
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	// assert.Equal(t, ExpectedResult, Result)
+	assert.Equal(t, "Hello httprouter", string(body))
+}
+
+func TestServeFileCatatan(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	// create testing
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/catatan.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	// assert.Equal(t, ExpectedResult, Result)
+	assert.Equal(t, "This is testing file", string(body))
+}
